c00276928bf3a1da0710c0f8ae2cfecf9fbd7fd5
BehindTimes <behindtimes@gmail.com>
2026-01-02 19:52:59 -0500

    Extract all the .16 images.

--- UltimaTileEditor/DataFiles.cs
+++ UltimaTileEditor/DataFiles.cs
@@ -14,9 +14,9 @@ namespace UltimaTileEditor
             "MON0", "MON1", "MON2", "MON3", "MON4", "MON5", "MON6", "MON7", "STARTSC",
             "STORY1", "STORY2", "STORY3", "STORY4", "STORY5", "STORY6", "TEXT", "TILES" ];*/
 
-        public static readonly List<string> Ultima5Files = [ "TILES",
-            "CREATE", "DNG1", "DNG2", "DNG3", "END1", "END2", "ENDSC",
-            "STORY1", "STORY2", "STORY3", "STORY4", "STORY5", "STORY6", "TEXT"];
+        public static readonly List<string> Ultima5Files = [ "CREATE", "DNG1", "DNG2", "DNG3", "END1", "END2", "ENDSC", "ITEMS",
+            "MON0", "MON1", "MON2", "MON3", "MON4", "MON5", "MON6", "MON7", "STARTSC",
+            "STORY1", "STORY2", "STORY3", "STORY4", "STORY5", "STORY6", "TEXT", "TILES" ];
 
         public static readonly List<string> Ultima5ImageFiles = [ "TILES" ];
 
--- UltimaTileEditor/Ultima5ImageExtractor.cs
+++ UltimaTileEditor/Ultima5ImageExtractor.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
 using System.Text;
+using System.Xml.Linq;
 
 namespace UltimaTileEditor
 {
@@ -34,7 +37,14 @@ namespace UltimaTileEditor
                         string? value = System.IO.Path.GetFileNameWithoutExtension(image);
                         if(value != null)
                         {
-                            CreateImages(lzw_out, strOutDir, value);
+                            if(value.ToLower().Contains("mon") || value.ToLower().Contains("items"))
+                            {
+                                CreateMaskImages(lzw_out, strOutDir, value);
+                            }
+                            else
+                            {
+                                CreateImages(lzw_out, strOutDir, value);
+                            }                
                         }
                     }
                 }
@@ -124,6 +134,148 @@ namespace UltimaTileEditor
                         }
 
                         string tempName = name + "_" + index.ToString() + ".png";
+                        string fullPath = Path.Combine(strOutDir, tempName);
+                        b.Save(fullPath, System.Drawing.Imaging.ImageFormat.Png);
+                    }
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("LZW file does not exist!");
+                    return;
+                }
+            }
+        }
+
+        private void AddMask(Bitmap b, byte[] lzw_out, int temp_offset, string name, string strOutDir)
+        {
+            int width = (lzw_out[temp_offset + 1] << 8) + (lzw_out[temp_offset + 0] << 0);
+            temp_offset += 2;
+            int height = (lzw_out[temp_offset + 1] << 8) + (lzw_out[temp_offset + 0] << 0);
+            temp_offset += 2;
+            if(width != b.Width)
+            {
+                MessageBox.Show("Invalid width!");
+                return;
+            }
+            if (height != b.Height)
+            {
+                MessageBox.Show("Invalid height!");
+                return;
+            }
+
+            // For masked images, all of them fall within the boundary, so this is never anything other than 0
+            // Leaving it in incase of custom modifications, but I'm unsure how Ultima 5 would treat this.
+            int bufwid = (8 - (width % 8)) % 8;
+
+            int data_size = (width + bufwid) * height / 8;
+
+            byte[] b8 = new byte[8];
+
+            try
+            {
+                int temp_width = 0;
+                int temp_height = 0;
+                for (int temp_index = 0; temp_index < data_size; temp_index++)
+                {
+                    byte curByte = lzw_out[temp_offset];
+                    for (int index = 0; index < 8; index++)
+                    {
+                        b8[index] = (byte)((curByte >> (7 - index)) & 0x01);
+                    }
+                    temp_offset++;
+
+                    for (int index = 0; index < 8; index++)
+                    {
+                        if (temp_width < width)
+                        {
+                            if (b8[index] != 0) // Set the alpha to 0
+                            {
+                                Color color1 = b.GetPixel(temp_width, temp_height);
+                                Color newColor = Color.FromArgb(0, color1.R, color1.G, color1.B);
+                                b.SetPixel(temp_width, temp_height, newColor);
+                            }
+                        }
+                        temp_width++;
+                        if (temp_width >= width + bufwid)
+                        {
+                            temp_width = 0;
+                            temp_height++;
+                        }
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("LZW file does not exist!");
+                return;
+            }
+        }
+
+        private void CreateMaskImages(byte[] lzw_out, string strOutDir, string name)
+        {
+            pngHelper helper = new pngHelper();
+
+            int temp_offset = 2;
+            int numImages = (lzw_out[1] << 8) + lzw_out[0];
+            numImages *= 2;
+            int[] offsets = new int[numImages];
+            for (int index = 0; index < numImages; index++)
+            {
+                offsets[index] = (lzw_out[temp_offset + 1] << 8) + (lzw_out[temp_offset + 0] << 0);
+                temp_offset += 2;
+            }
+            for (int index = 0; index < numImages; index += 2)
+            {
+                temp_offset = offsets[index];
+                int width = (lzw_out[temp_offset + 1] << 8) + (lzw_out[temp_offset + 0] << 0);
+                temp_offset += 2;
+                int height = (lzw_out[temp_offset + 1] << 8) + (lzw_out[temp_offset + 0] << 0);
+                temp_offset += 2;
+
+                int bufwid = (8 - (width % 8)) % 8;
+
+                int data_size = (width + bufwid) * height / 2;
+
+                try
+                {
+                    using (Bitmap b = new(width, height))
+                    {
+                        int temp_width = 0;
+                        int temp_height = 0;
+                        for (int temp_index = 0; temp_index < data_size; temp_index++)
+                        {
+                            byte curByte = lzw_out[temp_offset];
+                            byte pixel1 = (byte)((curByte >> 4) & 0x0F);
+                            byte pixel2 = (byte)(curByte & 0x0F);
+                            Color color1 = helper.GetColor(pixel1);
+                            Color color2 = helper.GetColor(pixel2);
+
+                            if (temp_width < width)
+                            {
+                                b.SetPixel(temp_width, temp_height, color1);
+                            }
+                            temp_width++;
+                            if (temp_width >= width + bufwid)
+                            {
+                                temp_width = 0;
+                                temp_height++;
+                            }
+                            if (temp_width < width)
+                            {
+                                b.SetPixel(temp_width, temp_height, color2);
+                            }
+                            temp_width++;
+                            if (temp_width >= width + bufwid)
+                            {
+                                temp_width = 0;
+                                temp_height++;
+                            }
+                            temp_offset++;
+                        }
+
+                        AddMask(b, lzw_out, offsets[index + 1], name, strOutDir);
+
+                        string tempName = name + "_" + (index / 2).ToString() + ".png";
                         string fullPath = Path.Combine(strOutDir, tempName);
                         b.Save(fullPath, System.Drawing.Imaging.ImageFormat.Png);
                     }
